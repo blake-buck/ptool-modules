70f745dee463f87cce5bfe3672ddd83b602bbcea
blake-buck <blakemanbuck@gmail.com>
2021-01-16 13:04:10 -0600

    functional route tests

--- firebase_auth/routes/authentication.test.js
+++ firebase_auth/routes/authentication.test.js
@@ -10,8 +10,10 @@ describe('Firebase authentication routes', () => {
     initializeStandardMiddleware(app);
     app.use(authenticationRouter);
 
-    const username='blake.buck@hey.com', email = username;
-    const password='temporaryPassword@1', previousPassword =password;
+    const username='blake.buck@hey.com';
+    const email = username
+    const password='temporaryPassword@1';
+    const previousPassword =password
     const proposedPassword ='temporaryPassword@2';
 
     let jwt='';
@@ -65,7 +67,7 @@ describe('Firebase authentication routes', () => {
             .post('/change-password')
             .set('Accept', 'application/json')
             .set('jwt', jwt)
-            .send({username, previousPassword, proposedPassword})
+            .send({previousPassword, proposedPassword})
             .expect('Content-Type', /json/)
             .expect(200)
             .end(async (err, res) => {
@@ -82,32 +84,9 @@ describe('Firebase authentication routes', () => {
 
     it('/forgot-password', async (done) => {
         request(app)
-        .post('/forgot-password')
-        .set('Accept', 'application/json')
-        .send({email})
-        .expect('Content-Type', /json/)
-        .expect(200)
-        .end(async (err, res) => {
-            if(err){
-                console.error(err);
-                console.log(res.error)
-                done();
-            }
-
-            expect(res.body.message).toBeTruthy();
-            done();
-        });
-    });
-
-    it('/delete-account', async (done) => {
-        const result = await firebase.client.signInWithEmailAndPassword(username, proposedPassword);
-        jwt = await result.user.getIdToken();
-        
-        request(app)
-            .post('/delete-account')
+            .post('/forgot-password')
             .set('Accept', 'application/json')
-            .set('jwt', jwt)
-            .send({})
+            .send({email})
             .expect('Content-Type', /json/)
             .expect(200)
             .end(async (err, res) => {
@@ -116,9 +95,51 @@ describe('Firebase authentication routes', () => {
                     console.log(res.error)
                     done();
                 }
-
                 expect(res.body.message).toBeTruthy();
+                const {uid} = await firebase.admin.getUserByEmail(email);
+                await firebase.admin.updateUser(uid, {password: proposedPassword});
                 done();
+            });
+    });
+
+    it('/delete-account', async (done) => {
+        
+        request(app)
+            .post('/login')
+            .set('Accept', 'application/json')
+            .send({
+                username, 
+                password: proposedPassword
+            })
+            .expect('Content-Type', /json/)
+            .end(async (err, res) => {
+                if(err){
+                    console.error(err);
+                    console.log(res.error)
+                    done();
+                }
+                else{
+                    console.log(res.body)
+
+                    request(app)
+                        .post('/delete-account')
+                        .set('Accept', 'application/json')
+                        .set('jwt', res.body.jwt)
+                        .send({})
+                        .expect('Content-Type', /json/)
+                        .expect(200)
+                        .end(async (err, res) => {
+                            if(err){
+                                console.error(err);
+                                console.log(res.error)
+                                done();
+                            }
+    
+                            expect(res.body.message).toBeTruthy();
+                            done();
+                        });
+                }
+                
             });
     });
 })
