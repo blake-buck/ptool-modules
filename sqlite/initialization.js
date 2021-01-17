64ba063d12a65905f35b0fb9ef11259fbeb0a2b9
blake-buck <blakemanbuck@gmail.com>
2021-01-17 10:05:08 -0600

    worked on sqlite open api spec

--- sqlite/initialization.js
+++ sqlite/initialization.js
@@ -2,10 +2,10 @@ const sqlite3 = require('sqlite3');
 const {DATABASE_FILE} = require('./config');
 
 let sqlite = {};
-function initializeDb(){
-    const db = new sqlite3.Database(DATABASE_FILE);
+function initializeDb(dbFile){
+    const db = new sqlite3.Database(dbFile ? dbFile : DATABASE_FILE);
     db.serialize();
-    database.db = db;
+    sqlite.db = db;
 }
 
 module.exports.initializeDb = initializeDb;
