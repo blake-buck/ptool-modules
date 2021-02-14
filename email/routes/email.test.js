c3677f38efeaccb677986f1c4323ed5de9f25f3e
blake-buck <blakemanbuck@gmail.com>
2021-02-14 15:51:08 -0600

    functional email route tests

--- email/routes/email.test.js
+++ email/routes/email.test.js
@@ -1,5 +1,43 @@
+const dependencyInjector = require('../dependency-injector');
+const request = require('supertest');
+const express = require('express');
+const nodemailer = require('nodemailer');
+
+dependencyInjector.register(
+    'emailTransport', 
+    () => nodemailer.createTransport({
+        jsonTransport:true
+    })
+)
+dependencyInjector.register('emailController', () => require('../controllers/email'));
+dependencyInjector.register('emailService', () => require('../services/email'));
+
+const {initializeStandardMiddleware} = require('../initialization');
+const emailRouter = require('./email');
+
+const app = express();
+initializeStandardMiddleware(app);
+app.use(emailRouter);
+
 describe('email route tests', () => {
-    it('should work', () => {
-        expect(true).toBe(true);
+    it('POST - /email', async (done) => {
+        request(app)
+            .post('/email')
+            .set('Accept', 'application/json')
+            .send({
+                to:'test@email.com',
+                subject:'A subject',
+                text:'test test test'
+            })
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end((err, response) => {
+                if(err) {
+                    console.log(err);
+                    return done();
+                }
+                expect(response.body.message).toBeTruthy();
+                done();
+            })
     })
 })
