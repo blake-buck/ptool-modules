601e351398371363bd06ec46946f6a72ab8305d0
blake-buck <blakemanbuck@gmail.com>
2021-02-15 15:39:16 -0600

    stripe service built out

--- stripe/services/stripe.js
+++ stripe/services/stripe.js
@@ -1,6 +1,8 @@
 const dependencyInjector = require('../dependency-injector');
 const stripe = dependencyInjector.inject('stripe');
 
+const {BadRequestError} = require('../constants/errors');
+
 const Joi = require('joi');
 
 const getCustomersValidation = Joi.object({
@@ -14,15 +16,32 @@ const getCustomersValidation = Joi.object({
     limit: Joi.number().integer().max(100).min(1).default(10),
     startingAfter: Joi.string()
 });
-async function getCustomers(){
+async function getCustomers(requestObj){
+    const {error, value} = getCustomersValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {email, limit, created, startingAfter} = value;
+    return await stripe.customers.list({
+        email,
+        limit,
+        created,
+        starting_after: startingAfter
+    })
 }
 
 const getSpecificCustomerValidation = Joi.object({
     customerId: Joi.string()
 });
-async function getSpecificCustomer(){
+async function getSpecificCustomer(requestObj){
+    const {error, value} = getSpecificCustomerValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {customerId} = value;
+    return await stripe.customers.retrieve(customerId);
 }
 
 const createCustomerValidation = Joi.object({
@@ -31,25 +50,64 @@ const createCustomerValidation = Joi.object({
     name: Joi.string().required(),
     phone:Joi.number().integer()
 });
-async function createCustomer(){
+async function createCustomer(requestObj){
+    const {error, value} = createCustomerValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
+    const {description, email, name, phone} = value;
 
+    return await stripe.customers.create({
+        description,
+        email,
+        name,
+        phone
+    })
 }
 
 const updateSpecificCustomerValidation = Joi.object({
+    customerId: Joi.string(),
     description: Joi.string().required(),
     email: Joi.string().email().required(),
     name: Joi.string().required(),
     phone:Joi.number().integer()
 });
-async function updateSpecificCustomer(){
+async function updateSpecificCustomer(requestObj){
+    const {error, value} = updateSpecificCustomerValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        customerId,
+        description,
+        email,
+        name,
+        phone
+    } = value;
+
+    return await stripe.customers.update(
+        customerId,
+        {
+            description, 
+            email,
+            name,
+            phone
+        }
+    )
 }
 
 const deleteSpecificCustomerValidation = Joi.object({
     customerId: Joi.string()
 });
-async function deleteSpecificCustomer(){
+async function deleteSpecificCustomer(requestObj){
+    const {error, value} = deleteSpecificCustomerValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {customerId} = value;
+    return await stripe.customers.del(customerId);
 }
 
 const createCardValidation = Joi.object({
@@ -71,16 +129,40 @@ const createCardValidation = Joi.object({
         country: Joi.string()
     })
 });
-async function createCard(){
+async function createCard(requestObj){
+    const {error, value} = createCardValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
+    const {customerId, source} = value;
 
+    return await stripe.customers.createSource(
+        customerId,
+        {
+            source:{
+                ...source,
+                object: 'card'
+            }
+        }
+    )
 }
 
 const getSpecificCardValidation = Joi.object({
     customerId: Joi.string(),
     cardId: Joi.string()
 });
-async function getSpecificCard(){
+async function getSpecificCard(requestObj){
+    const {error, value} = getSpecificCardValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {customerId, cardId} = value;
+
+    return await stripe.customers.retrieveSource(
+        customerId,
+        cardId
+    )
 }
 
 const updateSpecificCardValidation = Joi.object({
@@ -103,21 +185,48 @@ const updateSpecificCardValidation = Joi.object({
         country: Joi.string()
     })
 });
-async function updateSpecificCard(){
+async function updateSpecificCard(requestObj){
+    const {error, value} = updateSpecificCardValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
+    const {
+        customerId,
+        cardId,
+        source
+    } = value;
 
+    return stripe.customers.updateSource(
+        customerId,
+        cardId,
+        source
+    )
 }
 
 const deleteSpecificCardValidation = Joi.object({
     customerId: Joi.string(),
     cardId: Joi.string()
 });
-async function deleteSpecificCard(){
+async function deleteSpecificCard(requestObj){
+    const {error, value} = deleteSpecificCardValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        customerId,
+        cardId
+    } = value;
+
+    return await stripe.customers.delSource(
+        customerId,
+        cardId
+    )
 }
 
 const getSubscriptionsValidation = Joi.object({
     customer: Joi.string(),
-    price: Joi.string(),
+    priceId: Joi.string(),
     status:Joi.alternatives().try(
         'active',
         'past_due',
@@ -130,19 +239,41 @@ const getSubscriptionsValidation = Joi.object({
         'ended'
     )
 });
-async function getSubscriptions(){
+async function getSubscriptions(requestObj){
+    const {error, value} = getSubscriptionsValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        customer,
+        priceId,
+        status
+    } = value;
+
+    return await stripe.subscriptions.list({
+        customer,
+        price:priceId,
+        status
+    })
 }
 
 const getSpecificSubscriptionValidation = Joi.object({
     subscriptionId: Joi.string()
 });
-async function getSpecificSubscription(){
+async function getSpecificSubscription(requestObj){
+    const {error, value} = getSpecificSubscriptionValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {subscriptionId} = value;
+
+    return await stripe.subscriptions.retrieve(subscriptionId);
 }
 
 const createSubscriptionValidation = Joi.object({
-    customer: Joi.string(),
+    customerId: Joi.string(),
     items: Joi.array().items(
         Joi.object({
             price: Joi.string(),
@@ -150,11 +281,24 @@ const createSubscriptionValidation = Joi.object({
         })
     )
 });
-async function createSubscription(){
+async function createSubscription(requestObj){
+    const {error, value} = createSubscriptionValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
+    const {
+        customerId, 
+        items
+    } = value;
 
+    return await stripe.subscriptions.create({
+        customer: customerId,
+        items
+    })
 }
 
 const updateSpecificSubscriptionValidation = Joi.object({
+    subscriptionId: Joi.string(),
     items: Joi.array().items(
         Joi.object({
             price: Joi.string(),
@@ -163,15 +307,39 @@ const updateSpecificSubscriptionValidation = Joi.object({
     ),
     cancelAtEndOfCurrentPeriod: Joi.boolean()
 });
-async function updateSpecificSubscription(){
+async function updateSpecificSubscription(requestObj){
+    const {error, value} = updateSpecificSubscriptionValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        subscriptionId,
+        items,
+        cancelAtEndOfCurrentPeriod
+    } = value;
+
+    return await stripe.subscriptions.update(
+        subscriptionId,
+        {
+            items,
+            cancel_at_period_end: cancelAtEndOfCurrentPeriod
+        }
+    )
 }
 
 const deleteSpecificSubscriptionValidation = Joi.object({
     subscriptionId: Joi.string()
 });
-async function deleteSpecificSubscription(){
+async function deleteSpecificSubscription(requestObj){
+    const {error, value} = deleteSpecificSubscriptionValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {subscriptionId} = value;
+
+    return await stripe.subscriptions.del(subscriptionId);
 }
 
 
@@ -179,35 +347,90 @@ async function deleteSpecificSubscription(){
 const getProductsValidation = Joi.object({
     active: Joi.boolean()
 });
-async function getProducts(){
+async function getProducts(requestObj){
+    const {error, value} = getProductsValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {active} = value;
+
+    return await stripe.products.list({
+        active
+    })
 }
 const getSpecificProductValidation = Joi.object({
     productId: Joi.string()
 });
-async function getSpecificProduct(){
+async function getSpecificProduct(requestObj){
+    const {error, value} = getSpecificProductValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        productId
+    } = value;
+
+    return await stripe.products.retrieve(productId);
 }
 const createProductValidation = Joi.object({
     name: Joi.string().required(),
     description:Joi.string()
 });
-async function createProduct(){
+async function createProduct(requestObj){
+    const {error, value} = createProductValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        name,
+        description
+    } = value;
+
+    return await stripe.products.create({
+        name,
+        description
+    })
 }
 const updateSpecificProductValidation = Joi.object({
     productId: Joi.string(),
     name: Joi.string(),
     description: Joi.string()
 });
-async function updateSpecificProduct(){
+async function updateSpecificProduct(requestObj){
+    const {error, value} = updateSpecificProductValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        productId,
+        name,
+        description
+    } = value;
+
+    return await stripe.products.update(
+        productId,
+        {
+            name, 
+            description
+        }
+    )
 }
 const deleteSpecificProductValidation = Joi.object({
     productId: Joi.string()
 });
-async function deleteSpecificProduct(){
+async function deleteSpecificProduct(requestObj){
+    const {error, value} = deleteSpecificProductValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {productId} = value;
+
+    return await stripe.products.del(productId);
 }
 
 
@@ -218,34 +441,96 @@ const getPricesValidation = Joi.object({
     productId: Joi.string(),
     limit: Joi.number().integer().min(1).max(100).default(10)
 });
-async function getPrices(){
+async function getPrices(requestObj){
+    const {error, value} = getPricesValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        active,
+        currency,
+        productId,
+        limit
+    } = value;
+
+    return await stripe.prices.list({
+        active,
+        currency,
+        product: productId,
+        limit
+    })
 }
 const getSpecificPriceValidation = Joi.object({
     priceId: Joi.string()
 });
-async function getSpecificPrice(){
+async function getSpecificPrice(requestObj){
+    const {error, value} = getSpecificPriceValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {priceId} = value;
+
+    return await stripe.prices.retrieve(priceId);
 }
 const createPriceValidation = Joi.object({
     currency:Joi.string().length(3),
     unitAmount: Joi.number().integer(),
     productId: Joi.string()
 });
-async function createPrice(){
+async function createPrice(requestObj){
+    const {error, value} = createPriceValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        currency,
+        unitAmount,
+        productId
+    } = value;
+
+    return await stripe.prices.create({
+        currency,
+        unit_amount:unitAmount,
+        product: productId
+    })
 }
 const updateSpecificPriceValidation = Joi.object({
-    priceId: Joi.string()
+    priceId: Joi.string(),
+    active: Joi.boolean()
 });
-async function updateSpecificPrice(){
+async function updateSpecificPrice(requestObj){
+    const {error, value} = updateSpecificPriceValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {
+        priceId,
+        active
+    } = value;
+
+    return await stripe.prices.update(
+        priceId,
+        {
+            active
+        }
+    )
 }
 const deleteSpecificPriceValidation = Joi.object({
     priceId: Joi.string()
 });
-async function deleteSpecificPrice(){
+async function deleteSpecificPrice(requestObj){
+    const {error, value} = deleteSpecificPriceValidation.validate(requestObj);
+    if(error){
+        throw new BadRequestError(error);
+    }
 
+    const {priceId} = value;
+
+    return await stripe.prices.delete(priceId);
 }
 
 module.exports = {
